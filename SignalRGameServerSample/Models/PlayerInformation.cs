152c328e02887e849d5710a20bc82ae404e8fba0
MoRahemi <mo.rahemi.n@gmail.com>
2022-08-25 14:53:28 +0430

    fix

--- SignalRGameServerSample/Models/PlayerInformation.cs
+++ SignalRGameServerSample/Models/PlayerInformation.cs
@@ -1,12 +1,9 @@
-﻿using System.Text.Json.Serialization;
-
-namespace SignalRGameServerSample.Models
+﻿namespace SignalRGameServerSample.Models
 {
     public class PlayerInformation
     {
         public string Id { get; set; } = "";
         public string Name { get; set; } = "";
-        [JsonIgnore]
         public string GameId { get; set; } = "";
         public vector3 Position { get; set; } = vector3.Zero;
         public float LookAt { get; set; } = 0f;
--- SignalRGameServerSample/Models/Update.cs
+++ SignalRGameServerSample/Models/Update.cs
@@ -1,5 +1,4 @@
-﻿using System.Numerics;
-
+﻿
 namespace SignalRGameServerSample.Models
 {
     public class Update
